0d5ef97df240e8fc8d46a959dc043252912371d2
Clayton Hayes <claytonllibrar@users.noreply.github.com>
2018-05-01 12:47:59 -0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 var goButton = document.getElementById("go");
 goButton.onclick =function() {
 	var doi = document.getElementById("doi").value;
-	chrome.tabs.create({url:"https://dx.doi.org/"+doi});
+	chrome.tabs.create({url:"http://up7af9tu5s.search.serialssolutions.com/?rft_id=info:doi/"+doi});
 };
